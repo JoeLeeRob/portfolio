76ad4d810013db33062cec6e90403d06e4cfbfb3
Joe Robinson <joe.rob.dev@gmail.com>
2024-10-19 06:10:51 -0600

    Style changes for header

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,18 +67,19 @@ export default function RootLayout({
             </ul>
           </div>
           <div className="relative flex flex-col justify-center p-24">
-            <p className={`${orbitron.className} text-[5vw] leading-[6rem] xl:text-[5vw] xl:leading-[5vw] lg:text-[4vw] lg:leading-[6vw]`}
-            >
-              <span className="line-through">Joseph</span>
-            </p>
-            <p className={`${orbitron.className} 2xl:text-[10vw] 2xl:leading-[12rem] xl:text-[8vw] xl:leading-[8rem] lg:text-[7vw] lg:leading-[7vw]`}
-            >
-              Robinson
-            </p>
-            <span className={`${reenieBeanie.className} absolute transform rotate-[340deg] 2xl:text-[6rem] 2xl:top-[5%] 2xl:left-[32%] lg:top-[20%] lg:left-[35%] lg:text-[4rem] xl:top-[10%] xl:left-[37%] xl:text-[5rem]`}
+            <span className={`${reenieBeanie.className} absolute transform rotate-[340deg] 2xl:text-[6rem] 2xl:top-[5%] 2xl:left-[32%] lg:top-[20%] lg:left-[35%] lg:text-[3rem] xl:top-[10%] xl:left-[37%] xl:text-[5rem]`}
             >
               Joe
             </span>
+            <p className={`${orbitron.className} first-name text-[5vw] leading-[6rem] xl:text-[5vw] xl:leading-[5vw] lg:text-[4vw] lg:leading-[6vw]`}
+            >
+              <span className="line-through">Joseph</span>
+            </p>
+            <p className={`${orbitron.className} 2xl:text-[9vw] 2xl:leading-[9vw] xl:text-[8vw] xl:leading-[8rem] lg:text-[7vw] lg:leading-[7vw]`}
+            >
+              Robinson
+            </p>
+            <p className="2xl:text-[2rem] 2xl:leading-[2rem] xl:text-[1.75rem] xl:leading-[1.75rem] lg:text-[1.25rem] lg:leading-[1rem]">Software Developer</p>
           </div>
           {/* <blockquote><p>"Code is like humor. When you have to explain it, it's bad" -Cory House</p></blockquote> */}
         </header>
